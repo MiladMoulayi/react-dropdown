6a4521c5541ddeba547ca59f5ee20e162faed6f0
MiladMoulayi <m.moulayi123@gmail.com>
2024-01-08 17:16:23 -0800

    more changes to make it cleaner

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -5,14 +5,25 @@ import list from "./list.json";
 
 function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentSelection, setCurrentSelection] = useState({
+    text: "Pick your poison",
+    emoticon: "😈",
+  });
   return (
     <div className="relative flex flex-col items-center w-[340px] h-[340px] rounded-lg">
       <button
         onClick={() => setIsOpen((isOpen) => !isOpen)}
-        className="bg-blue-400 p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
+        className="bg-blue-400 p-4 w-full flex items-center font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white"
       >
-        Dropdown
-        {!isOpen ? <FaChevronDown /> : <FaChevronUp />}
+        <div className="flex w-full justify-between">
+          <div>{currentSelection.text}</div>
+          <div>{currentSelection.emoticon}</div>
+        </div>
+        {!isOpen ? (
+          <FaChevronDown className="ml-2" />
+        ) : (
+          <FaChevronUp className="ml-2" />
+        )}
       </button>
       {isOpen && (
         <div className="bg-blue-400 absolute top-20 flex flex-col items-start rounded-lg p-2 w-full">
@@ -20,6 +31,13 @@ function Dropdown() {
             <div
               className="flex w-full justify-between hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4"
               key={i}
+              onClick={() => {
+                setCurrentSelection({
+                  text: item.city,
+                  emoticon: item.emoticon,
+                });
+                setIsOpen(false);
+              }}
             >
               <h3>{item.city}</h3>
               <h3>{item.emoticon}</h3>
